fe0dfc22f18e939ee27c9d42bf724501caa13fe3
LebedievaHelen <115153268+LebedievaHelen@users.noreply.github.com>
2022-10-07 22:18:29 +0300

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,17 @@
+public class Book {
+    private String name;
+    private int year;
+
+    public Book(String name, int year) {
+        this.name = name;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+}
