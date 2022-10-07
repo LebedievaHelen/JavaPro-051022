d609f724ce1ff54e4647cc476d33525dfd490df0
LebedievaHelen <115153268+LebedievaHelen@users.noreply.github.com>
2022-10-07 22:19:01 +0300

    Add files via upload

---
+++ Country.java
@@ -0,0 +1,11 @@
+public class Country {
+    private String nameCountry;
+
+    public String getNameCountry() {
+        return nameCountry;
+    }
+
+    public Country(String getNameCountry) {
+        this.nameCountry = getNameCountry;
+    }
+}
